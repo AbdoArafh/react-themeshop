bafa13f6d5914dc9477bd0c9410917f11e30da1b
AbdoArafh <abdoarafh54@gmail.com>
2022-02-21 15:36:05 +0200

    orbits working but needs actual images

--- src/pages/themes/orbits.jsx
+++ src/pages/themes/orbits.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import Vec2D from '../../utils/vectors.js';
+import { round } from '../../utils/general';
 
 function useAnimationFrame(callback) {
     const requestRef = useRef();
@@ -18,7 +19,38 @@ function useAnimationFrame(callback) {
     }, []);
 }
 
-function Orbit({placements, size}) {
+function OrbitingIcon({data, width, height, direction}) {
+
+    const [size, setSize] = useState({w: 0, h: 0});
+    const iconRef = useRef(null);
+
+    useEffect(
+        () => {
+            setSize({w: iconRef.current.clientWidth, h: iconRef.current.clientHeight});
+        }
+    , []);
+
+
+    function getX(dir) {
+        return (round((width/2) * Math.cos(direction + dir), 3)) + (width/2) - (size.w/2);
+    }
+
+    function getY(dir) {
+        return (round((height/2) * Math.sin(direction + dir), 3)) + (height/2) - (size.h/2);
+    }
+
+    return (
+        <div
+            key={data.placement}
+            className="icon absolute top-0 left-0 z-10"
+            style={{transform: `translate(${getX(data.direction)}px, ${getY(data.direction)}px)`}}
+            ref={iconRef}
+        >
+        </div>
+    )
+}
+
+function Orbit({icons, size, speed=1}) {
 
     const directions = {
         top: new Vec2D(0, -1),
@@ -27,13 +59,14 @@ function Orbit({placements, size}) {
         right: new Vec2D(1, 0) 
     }
         
-    const [iconsData, setIconsData] = useState([
-        {
-            placement: "top",
-            direction: directions["top"].heading(),
-            pos: new Vec2D()
-        }
-    ]);
+    const [iconsData] = useState(icons.map(
+        icon => ({
+            placement: icon.placement,
+            direction: directions[icon.placement].heading(),
+            pos: new Vec2D(),
+            icon: icon.icon
+        })
+    ));
 
     const [direction, setDirection] = useState(0);
 
@@ -41,6 +74,7 @@ function Orbit({placements, size}) {
 
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
+
     useEffect(
         () => {
             setWidth(orbitRef.current.clientWidth);
@@ -49,28 +83,15 @@ function Orbit({placements, size}) {
     , []);
 
     useAnimationFrame(deltaTime => {
-        setDirection(prevDirection => (prevDirection + 0.001 * deltaTime) % (Math.PI * 2));
+        setDirection(prevDirection => (prevDirection + (0.001 * deltaTime * speed)) % (Math.PI * 2));
     });
-
-    function getX() {
-        return (Math.round((width/2) * Math.cos(direction))) + (width/2);
-    }
-
-    function getY() {
-        return (Math.round((height/2) * Math.sin(direction))) + (height/2);
-    }
     
     return (
         <div className="center">
             <div ref={orbitRef} className={`orbit orbit-${size}`}>
                 {iconsData.map(
-                    icon => (
-                        <div
-                            key={icon.placement}
-                            className={`icon absolute top-0 left-0 z-10`}
-                            style={{transform: `translate(${getX()}px, ${getY()}px)`}}
-                        >
-                        </div>
+                    data => (
+                        <OrbitingIcon key={data.placement} data={data} width={width} height={height} direction={direction} />
                     )
                 )}
             </div>
@@ -79,20 +100,22 @@ function Orbit({placements, size}) {
 }
 
 export default function Orbits() {
-    const orbitRef = useRef(null);
-    const [width, setWidth] = useState(0);
-    const [height, setHeight] = useState(0);
-    useEffect(
-        () => {
-            setWidth(orbitRef.current.clientWidth);
-            setHeight(orbitRef.current.clientHeight);
-        }
-    , []);
     return (
-		<div ref={orbitRef} className="orbits">
-            <Orbit width={width} height={height} placements={["top", "bottom"]} size="sm"/>
-            <Orbit width={width} height={height} placements={["left", "right"]} size="md"/>
-            <Orbit width={width} height={height} placements={["top", "bottom", "left", "right"]} size="lg"/>
+		<div className="orbits">
+            <Orbit icons={[
+                    {placement: "top"},
+                    {placement: "bottom"}
+                ]} size="sm" speed={3} />
+            <Orbit icons={[
+                    {placement: "left"},
+                    {placement: "right"}
+                ]} size="md"/>
+            <Orbit icons={[
+                    {placement: "top"},
+                    {placement: "bottom"},
+                    {placement: "left"},   
+                    {placement: "right"}   
+                ]} size="lg"/>
         </div>
     )
 }
--- src/utils/general.js
+++ src/utils/general.js
@@ -1,3 +1,8 @@
 export function capitalizeFirstLetter(str) {
     return str[0].toUpperCase().concat(str.slice(1));
 }
+
+export function round(num,pre=0) {
+    const pow = Math.pow(10,pre);
+    return Math.round(num*pow)/pow;
+}
