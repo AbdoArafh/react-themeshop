b898d6f4c413fde7d6519c3cd5f0991123e2d26a
Abdurrahman <abdoarafh54@gmail.com>
2022-08-13 13:13:37 +0200

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import Themes from './pages/themes'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
-import FAQs from './pages/FAQs'
-import ProductDetails from './pages/Product_Details'
-import { ProductsArray as products } from './utils/readyProducts'
-import ShoppingCart from './pages/Shopping_Cart'
-import AccountDashboard from './pages/Account_Dashboard'
-import AllProducts from './pages/Products'
+import React from "react";
+import ReactDOM from "react-dom";
+
+// styles
+import "./index.css";
+
+// components
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// utils
+import { ProductsArray as products } from "./utils/readyProducts";
+
+// pages
+import ProductDetails from "./pages/Product_Details";
+import Themes from "./pages/themes";
+import FAQs from "./pages/FAQs";
+import ShoppingCart from "./pages/Shopping_Cart";
+import AccountDashboard from "./pages/Account_Dashboard";
+import AllProducts from "./pages/Products";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,17 +26,26 @@ ReactDOM.render(
       <Navbar />
       <Routes>
         <Route index element={<Themes products={products} />} />
-        <Route path="/faqs" element={<FAQs />}/>
-        <Route path="/product-details/:id" element={<ProductDetails products={products} />} />
+        <Route path="/faqs" element={<FAQs />} />
+        <Route
+          path="/product-details/:id"
+          element={<ProductDetails products={products} />}
+        />
         <Route path="/shopping-cart" element={<ShoppingCart />} />
         <Route path="/account-dashboard" element={<AccountDashboard />} />
         {/* Fun fact: It's actually recommended starting from v6 of react router */}
         {/* to add optional parameters */}
-        <Route path="/all-products/:tags" element={<AllProducts products={products} />} />
-        <Route path="/all-products" element={<AllProducts products={products} />} />
+        <Route
+          path="/all-products/:tags"
+          element={<AllProducts products={products} />}
+        />
+        <Route
+          path="/all-products"
+          element={<AllProducts products={products} />}
+        />
       </Routes>
       <Footer />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
